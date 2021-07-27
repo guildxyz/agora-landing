4d786919b1d27bd662f10233b386ea9f846e5a4c
KovJonas <kovacsjonas99@gmail.com>
2021-07-27 12:39:00 +0200

    Roadmap section

---
+++ src/lib/sections/Roadmap/components/Step.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let title;
+  export let q;
+  export let content;
+</script>
+
+<div
+  class="flex flex-col justify-center items-center space-y-8 relative px-8 py-16 border-2 border-[#FDF7FF] bg-[#2E2093]"
+>
+  <h3 class="relative text-[#E77FF0] text-3xl font-bold tracking-tight font-display uppercase">
+    {title}<span class="absolute -top-3 -right-2 w-4 h-4 text-base font-semibold">{q}</span>
+  </h3>
+
+  <p class="text-[#FDF7FF] font-semibold text-xl">{content}</p>
+
+  <div class="absolute top-1/3 -right-20 w-20 h-0.5 bg-[#FDF7FF]" />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,10 +3,12 @@
   import WhatIsAgora from '$lib/sections/WhatIsAgora.svelte';
   import GreatThinkers from '$lib/sections/GreatThinkers';
   import WhoIsAgoraFor from '$lib/sections/WhoIsAgoraFor.svelte';
+  import Roadmap from '$lib/sections/Roadmap';
 
   import Footer from '$lib/Footer.svelte';
 </script>
 
+<Roadmap />
 <Hero />
 <WhatIsAgora />
 <GreatThinkers />
