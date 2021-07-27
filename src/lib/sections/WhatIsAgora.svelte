f1600d1eaafeed35d716fc422f9facb00be3e15a
KovJonas <kovacsjonas99@gmail.com>
2021-07-27 08:40:33 +0200

    "What is Agora Space" - responsiveness

--- src/lib/sections/WhatIsAgora.svelte
+++ src/lib/sections/WhatIsAgora.svelte
@@ -1,13 +1,13 @@
 <section id="what-is-agora-space" class="relative bg-[#4F1D85]">
   <div
-    class="flex justify-end container mx-auto lg:px-8 pt-8 md:pt-20 lg:pt-40 pb-72 lg:pb-40 text-[#FDF7FF]"
+    class="flex justify-end container mx-auto md:px-8 pt-8 md:pt-20 xl:pt-40 pb-72 lg:pb-20 xl:pb-40 text-[#FDF7FF]"
   >
     <div
       class="absolute left-0 bottom-0 w-full md:w-1/2 lg:w-2/5 max-w-lg h-full bg-what-is-agora-space bg-contain bg-no-repeat bg-left-bottom"
     />
     <div class="w-full lg:w-3/5 text-center">
-      <h2 class="mb-8 text-4xl lg:text-6xl font-bold tracking-tight font-display uppercase">
-        What is <span class="block md:inline">Agora Space?</span>
+      <h2 class="mb-8 text-4xl xl:text-6xl font-bold tracking-tight font-display uppercase">
+        What is <br class="md:hidden" />Agora Space?
       </h2>
       <p class="lg:font-semibold text-lg md:text-xl">
         A framework for all social token tools. A broad service pack elevating tokenized
