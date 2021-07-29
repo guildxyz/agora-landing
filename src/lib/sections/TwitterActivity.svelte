4fa7d718d3670c941afa3135cc739bf7a54561cf
KovJonas <kovacsjonas99@gmail.com>
2021-07-29 11:33:42 +0200

    "Role management bot" section + responsiveness

---
+++ src/lib/sections/TwitterActivity.svelte
@@ -0,0 +1,26 @@
+<section id="gamification-of-twitter-activity" class="relative bg-[#A34BAB] text-[#FDF7FF]">
+  <div class="container mx-auto py-20 px-8 grid lg:grid-cols-2 gap-16 lg:gap-0">
+    <!-- Image -->
+    <div class="order-2 lg:order-1 flex items-center justify-center lg:pr-20">
+      <img
+        src="/images/twitter-activity.png"
+        alt="Gamification Of Twitter Activity"
+        class="mx-auto max-w-sm sm:max-w-md md:max-w-lg lg:max-w-full"
+      />
+    </div>
+
+    <!-- Text -->
+    <div class="order-1 lg:order-2 flex flex-col items-start justify-center space-y-8">
+      <h2 class="mb-8 text-4xl 2xl:text-7xl font-bold tracking-tight font-display uppercase">
+        Role management bot
+      </h2>
+
+      <p class="text-lg 2xl:text-xl">
+        Our bot Hermes bridges Telegram and Discord channels and his ally Medusa guards your token
+        gated communities. You can invite the bots to your channels with the press of a button and
+        Medusa will manage all affairs of admitting and dismissing people from your groups based on
+        how much they hold or stake of your token.
+      </p>
+    </div>
+  </div>
+</section>
--- src/routes/tools.svelte
+++ src/routes/tools.svelte
@@ -1,7 +1,9 @@
 <script>
   import RoleManagementBot from '$lib/sections/RoleManagementBot.svelte';
+  import TwitterActivity from '$lib/sections/TwitterActivity.svelte';
   import Footer from '$lib/Footer.svelte';
 </script>
 
 <RoleManagementBot />
+<TwitterActivity />
 <Footer purple />
