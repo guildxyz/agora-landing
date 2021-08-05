a6186666507f5f7668def54d0141727d8e58b9b3
KovJonas <kovacsjonas99@gmail.com>
2021-08-05 08:51:49 +0200

    Hero - "oracle" color fix

--- src/lib/sections/Hero/Hero.svelte
+++ src/lib/sections/Hero/Hero.svelte
@@ -80,7 +80,7 @@
           <h2
             class="mb-4 lg:mb-8 font-bold tracking-tight text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-display"
           >
-            Social <span class="text-agora-blue-bg">oracle</span> <br />and token utility
+            Social <span class="text-agora-blue-medium">oracle</span> <br />and token utility
             <br />toolkit
           </h2>
 
