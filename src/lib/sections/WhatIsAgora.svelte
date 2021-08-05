19feae83b7ef2a4b13d9c2568c99a1627214fb4f
KovJonas <kovacsjonas99@gmail.com>
2021-08-05 20:18:37 +0200

    What is Agora Space - without absolute positioning

--- src/lib/sections/WhatIsAgora.svelte
+++ src/lib/sections/WhatIsAgora.svelte
@@ -1,19 +1,15 @@
 <section id="what-is-agora-space" class="relative bg-agora-purple">
   <div class="relative w-full max-fullhd-container mx-auto flex flex-col">
-    <div
-      class="flex flex-col lg:flex-row justify-end px-8 pt-16 md:pt-20 lg:py-28 xl:py-48 text-agora-white"
-    >
+    <div class="flex flex-col lg:flex-row items-start lg:items-end px-8 text-agora-white">
       <div
-        class="order-2 lg:order-1 lg:absolute lg:left-0 lg:bottom-0 flex flex-col justify-end h-full w-full lg:w-1/2 pt-8 lg:pr-8 -mx-8"
+        class="order-2 lg:order-1 flex flex-col justify-end h-full w-full lg:w-1/2 pt-16 lg:pr-8 -mx-8"
       >
-        <img
-          src="/images/what-is-agora-space-bg.png"
-          alt="What is Agora Space?"
-          class="w-full h-full pt-16 object-cover lg:object-contain object-left-bottom"
-        />
+        <img src="/images/what-is-agora-space-bg.png" alt="What is Agora Space?" class="w-full" />
       </div>
 
-      <div class="order-1 lg:order-2 container lg:mr-0 lg:w-3/5 lg:pl-20 text-center">
+      <div
+        class="order-1 lg:order-2 container lg:mr-0 pt-16 md:pt-20 lg:py-28 2xl:py-48 lg:max-w-3/5 text-center"
+      >
         <h2
           class="mb-8 text-3xl lg:text-5xl 2xl:text-6xl font-bold tracking-tight font-display uppercase"
         >
