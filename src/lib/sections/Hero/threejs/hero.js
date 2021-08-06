cf392b51f32e31eaeeb8bf8b80323186aa264a3c
KovJonas <kovacsjonas99@gmail.com>
2021-08-06 19:17:50 +0200

    Three.js - loading texture more efficiently

--- src/lib/sections/Hero/threejs/hero.js
+++ src/lib/sections/Hero/threejs/hero.js
@@ -61,56 +61,56 @@ export const initThreeJS = (element, callback) => {
 
   // Texture
   const textureLoader = new THREE.TextureLoader();
-  const matCapTexture = textureLoader.load('/images/bubble-matcap.png');
+  textureLoader.load('/images/bubble-matcap.png', (matCapTexture) => {
+    // Object
+    bubbleGeometry = new THREE.IcosahedronGeometry(0.8, 64);
+    bubbleMaterial = new THREE.MeshMatcapMaterial({
+      matcap: matCapTexture
+    });
 
-  // Object
-  bubbleGeometry = new THREE.IcosahedronGeometry(0.8, 64);
-  bubbleMaterial = new THREE.MeshMatcapMaterial({
-    matcap: matCapTexture
-  });
+    // Modifying the vertex shader
+    bubbleMaterial.onBeforeCompile = (shader) => {
+      shader.uniforms.uTime = customUniforms.uTime;
+      shader.uniforms.uSpeed = customUniforms.uSpeed;
+      shader.uniforms.uNoiseDensity = customUniforms.uNoiseDensity;
+      shader.uniforms.uNoiseStrength = customUniforms.uNoiseStrength;
 
-  // Modifying the vertex shader
-  bubbleMaterial.onBeforeCompile = (shader) => {
-    shader.uniforms.uTime = customUniforms.uTime;
-    shader.uniforms.uSpeed = customUniforms.uSpeed;
-    shader.uniforms.uNoiseDensity = customUniforms.uNoiseDensity;
-    shader.uniforms.uNoiseStrength = customUniforms.uNoiseStrength;
-
-    // Adding the necessary code to the vertex shader's header
-    shader.vertexShader = shader.vertexShader.replace(
-      '#include <common>',
-      `
-
-          #include <common>
-  
-          ${vertexShaderHead}
+      // Adding the necessary code to the vertex shader's header
+      shader.vertexShader = shader.vertexShader.replace(
+        '#include <common>',
         `
-    );
-
-    shader.vertexShader = shader.vertexShader.replace(
-      '#include <fog_vertex>',
-      `
-          #include <fog_vertex>
   
-          ${vertexShaderBody}
+            #include <common>
+    
+            ${vertexShaderHead}
+          `
+      );
+
+      shader.vertexShader = shader.vertexShader.replace(
+        '#include <fog_vertex>',
         `
-    );
-  };
+            #include <fog_vertex>
+    
+            ${vertexShaderBody}
+          `
+      );
+    };
 
-  bubble = new THREE.Mesh(bubbleGeometry, bubbleMaterial);
-  bubble.position.set(0.2, 1.5, 0);
-  bubble.scale.set(1, 0.8, 1);
-  scene.add(bubble);
+    bubble = new THREE.Mesh(bubbleGeometry, bubbleMaterial);
+    bubble.position.set(0.2, 1.5, 0);
+    bubble.scale.set(1, 0.8, 1);
+    scene.add(bubble);
 
-  // Renderer
-  renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    alpha: true,
-    canvas: element
+    // Renderer
+    renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true,
+      canvas: element
+    });
+    renderer.setClearColor(0x000000, 0);
+
+    resizeThreeJS(rect.width, rect.height);
+    animate();
+    callback();
   });
-  renderer.setClearColor(0x000000, 0);
-
-  resizeThreeJS(rect.width, rect.height);
-  animate();
-  callback();
 };
