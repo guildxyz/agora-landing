032a4487b44f1d280610980c48403e8979a49329
KovJonas <kovacsjonas99@gmail.com>
2021-09-02 11:10:40 +0200

    Who is agora for - bg color update

--- src/lib/sections/WhoIsAgoraFor.svelte
+++ src/lib/sections/WhoIsAgoraFor.svelte
@@ -9,7 +9,7 @@
   <div
     class="relative w-full custom-container lg:grid lg:grid-cols-5 h-full overflow-hidden md:overflow-visible"
   >
-    <div class="relative lg:col-span-2 bg-agora-purple">
+    <div class="relative lg:col-span-2 bg-agora-purple lg:bg-agora-gray">
       <!-- Background circle left -->
       <div
         class="absolute bottom-0 left-0 w-[50vw] h-[50vw] md:w-[25vw] md:h-[25vw] transform -translate-x-1/2 translate-y-1/2 bg-circle-pattern bg-no-repeat bg-cover"
