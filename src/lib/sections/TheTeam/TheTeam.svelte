a846e3e2c07e6791817faf3de56542ec44afcd74
KovJonas <kovacsjonas99@gmail.com>
2021-09-02 12:34:13 +0200

    "Advisors" section redesign

--- src/lib/sections/TheTeam/TheTeam.svelte
+++ src/lib/sections/TheTeam/TheTeam.svelte
@@ -91,18 +91,61 @@
             class="grid grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-16 xl:gap-32 max-w-xs lg:max-w-none"
           >
             <TeamMember
+              textAlign="left"
               photo="/images/team/raz.png"
               name="R A Z"
               label="Strategic advisor"
               twitter="https://twitter.com/L1sp3r"
+              links={[
+                {
+                  text: 'Supernova Fund',
+                  url: 'https://supernova.fund'
+                }
+              ]}
             />
-            <TeamMember photo="/images/team/petermm.png" name="Peter MM" label="Advisor" />
-            <TeamMember photo="/images/team/d3v.png" name="D3v" label="Security advisor" />
             <TeamMember
+              textAlign="left"
+              photo="/images/team/petermm.png"
+              name="Peter MM"
+              label="Advisor"
+              links={[
+                {
+                  text: 'Supernova Fund',
+                  url: 'https://supernova.fund'
+                },
+                {
+                  text: 'Prev.: J.P. Morgan'
+                }
+              ]}
+            />
+            <TeamMember
+              textAlign="left"
+              photo="/images/team/d3v.png"
+              name="D3v"
+              label="Security advisor"
+              links={[
+                {
+                  text: 'Former SSNS NCSC',
+                  url: 'https://mi.nemzetilabor.hu/partners/special-service-national-security'
+                }
+              ]}
+            />
+            <TeamMember
+              textAlign="left"
               photo="/images/team/nima.png"
               name="NiMA"
               label="Advisor"
               twitter="https://twitter.com/insidenima"
+              links={[
+                {
+                  text: 'Seedclub',
+                  url: 'https://seedclub.xyz'
+                },
+                {
+                  text: 'Mintbase',
+                  url: 'https://www.mintbase.io'
+                }
+              ]}
             />
           </div>
         </svelte:component>
--- src/lib/sections/TheTeam/components/TeamMembers.svelte
+++ src/lib/sections/TheTeam/components/TeamMembers.svelte
@@ -1,22 +1,61 @@
 <script>
+  import { Link, TwitterLogo } from 'phosphor-svelte';
+
+  export let textAlign = 'center';
   export let photo;
   export let name;
   export let label;
   export let twitter = null;
+  export let links = []; // format: [{ text: string, url?: string }, ...]
 </script>
 
 <div class="flex flex-col">
   <div class="flex flex-col items-end mb-2 h-52 lg:h-64">
     <img src={photo} alt={name} class="w-full h-full object-contain" />
   </div>
-  <span class="text-agora-pink-light text-xl font-bold tracking-tight font-display">
+  <span
+    class={`flex ${
+      textAlign === 'left' ? 'justify-between' : 'justify-center'
+    } items-center space-x-2 text-agora-pink-light text-xl font-bold tracking-tight font-display`}
+  >
     {#if twitter}
-      <a href={twitter} target="_blank" rel="noopener" class="transition hover:text-agora-white"
-        >{name}</a
-      >
+      <span>{name}</span>
+      {#if twitter}
+        <a
+          href={twitter}
+          target="_blank"
+          alt={`${name} - Twitter`}
+          class="flex items-center justify-center w-5 h-5 bg-white rounded-sm text-agora-gray transition-all duration-500 delay-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-300"
+        >
+          <TwitterLogo size={16} />
+        </a>
+      {/if}
     {:else}
       {name}
     {/if}
   </span>
-  <span class="text-agora-white font-display font-light">{label}</span>
+  <span
+    class={`text-agora-white font-display font-light ${
+      textAlign === 'left' ? 'text-left' : 'text-center'
+    }`}>{label}</span
+  >
+
+  {#if links.length > 0}
+    <div class="mt-2 flex flex-col space-y-1 text-agora-pink-light font-semibold">
+      {#each links as link}
+        <span class="flex items-center justify-start space-x-2 text-left">
+          {#if link.url}
+            <a href={link.url} target="_blank">
+              {link.text}
+            </a>
+            <span class="hidden lg:inline">
+              <Link />
+            </span>
+          {:else}
+            {link.text}
+          {/if}
+        </span>
+      {/each}
+    </div>
+  {/if}
 </div>
