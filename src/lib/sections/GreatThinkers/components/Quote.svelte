d55ee660b56c403f87086da0121425034ecf04ae
KovJonas <kovacsjonas99@gmail.com>
2021-09-02 11:09:30 +0200

    Updated quotes

--- src/lib/sections/GreatThinkers/components/Quote.svelte
+++ src/lib/sections/GreatThinkers/components/Quote.svelte
@@ -1,16 +1,18 @@
 <script>
   export let image;
   export let alt;
+  export let quoteStart;
   export let quote;
   export let name;
   let _class = null;
   export { _class as class };
 </script>
 
-<div class={`flex flex-col items-center text-md ${_class}`}>
+<div class={`mx-auto max-w-sm flex flex-col items-center text-md ${_class}`}>
   <img src={image} {alt} class="mb-8 h-52 2xl:h-72" />
   <p class="mb-3 text-agora-white lg:font-semibold 2xl:text-lg">
-    “{quote}”
+    “<span class="text-agora-pink-light">{quoteStart}</span><br />
+    {quote}”
   </p>
   <p class="mt-auto text-lg text-agora-pink-light font-bold tracking-tight font-display">
     -{name}-
