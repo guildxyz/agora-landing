5d912daef69d6c4b8ab77d2a2630b8c3443c0ef0
KovJonas <kovacsjonas99@gmail.com>
2021-09-02 17:20:54 +0200

    Tools page - UI fixes

--- src/lib/sections/Gamification.svelte
+++ src/lib/sections/Gamification.svelte
@@ -4,7 +4,7 @@
   >
     <!-- Left side -->
     <div
-      class="group relative flex flex-col items-center lg:items-start justify-center lg:col-span-3 xl:col-span-2 px-8 lg:pr-20 pt-24 lg:pt-40 pb-10 h-full text-agora-white text-center lg:text-left"
+      class="group relative flex flex-col items-center lg:items-start justify-center lg:col-span-3 xl:col-span-2 px-8 lg:pr-20 pt-24 lg:pt-0 pb-10 h-full text-agora-white text-center lg:text-left"
     >
       <span
         class="mb-2 w-full text-lg 2xl:text-2xl font-bold tracking-tight font-display uppercase opacity-100 lg:opacity-0 group-hover:opacity-100 transition-all duration-300 transform lg:translate-y-8 group-hover:translate-y-0"
@@ -24,7 +24,7 @@
 
     <!-- Right Side -->
     <div
-      class="flex flex-col items-start justify-center space-y-4 lg:col-span-3 relative px-20 lg:pt-40 pb-10 lg:pb-20 pr-8 pl-10 lg:pl-20 xl:pl-40 text-agora-white"
+      class="flex flex-col items-center justify-center space-y-4 lg:col-span-3 relative px-20 lg:pt-24 pb-10 lg:pb-20 pr-8 pl-10 lg:pl-20 xl:pl-40 text-agora-white"
     >
       <img src="/images/gamification.png" alt="Role management bot" class="max-w-full" />
     </div>
