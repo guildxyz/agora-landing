09b8075ae44f5b2f9d56f8522ef96daf9d7fa33c
KovJonas <kovacsjonas99@gmail.com>
2021-09-03 21:37:40 +0200

    Small refactor

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -5,32 +5,35 @@ export async function get() {
     'Cache-Control': 'max-age=0, s-maxage=3600',
     'Content-Type': 'application/xml'
   };
+
+  const body = `<?xml version="1.0" encoding="UTF-8" ?>
+  <urlset
+    xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+  >
+    <url>
+      <loc>https://agora.space/</loc>
+      <lastmod>${buildDate}</lastmod>
+      <priority>1.00</priority>
+    </url>
+    <url>
+      <loc>https://agora.space/explorer</loc>
+      <lastmod>${buildDate}</lastmod>
+      <priority>0.80</priority>
+    </url>
+    <url>
+      <loc>https://agora.space/tools</loc>
+      <lastmod>${buildDate}</lastmod>
+      <priority>0.80</priority>
+    </url>
+    <url>
+      <loc>https://agora.space/about</loc>
+      <lastmod>${buildDate}</lastmod>
+      <priority>0.80</priority>
+    </url>
+  </urlset>`;
+
   return {
     headers,
-    body: `<?xml version="1.0" encoding="UTF-8" ?>
-    <urlset
-      xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-    >
-      <url>
-        <loc>https://agora.space/</loc>
-        <lastmod>${buildDate}</lastmod>
-        <priority>1.00</priority>
-      </url>
-      <url>
-        <loc>https://agora.space/explorer</loc>
-        <lastmod>${buildDate}</lastmod>
-        <priority>0.80</priority>
-      </url>
-      <url>
-        <loc>https://agora.space/tools</loc>
-        <lastmod>${buildDate}</lastmod>
-        <priority>0.80</priority>
-      </url>
-      <url>
-        <loc>https://agora.space/about</loc>
-        <lastmod>${buildDate}</lastmod>
-        <priority>0.80</priority>
-      </url>
-    </urlset>`
+    body
   };
 }
