d645ffe52aa0fbdf277339726c17a554635aa34b
KovJonas <kovacsjonas99@gmail.com>
2021-09-03 11:36:18 +0200

    Added content to the "Agora Space DAO" section

--- src/lib/sections/AgoraSpaceDao/AgoraSpaceDao.svelte
+++ src/lib/sections/AgoraSpaceDao/AgoraSpaceDao.svelte
@@ -1,3 +1,25 @@
-<section id="agora-space-dao" class="relative h-screen bg-agora-gray text-center">
-  <canvas class="absolute inset-0 w-full h-full" />
+<script>
+  import { onMount } from 'svelte';
+  import { initThreeJS } from '$lib/threejs/agoraSpaceDao';
+
+  let canvas;
+
+  onMount(() => {
+    initThreeJS(canvas);
+  });
+</script>
+
+<section id="agora-space-dao" class="relative h-screen bg-agora-gray">
+  <canvas bind:this={canvas} class="absolute inset-0 w-full h-full" />
+  <div class="container px-8 max-w-screen-md text-center">
+    <h2
+      class="mt-24 mb-12 text-3xl lg:text-4xl 2xl:text-5xl font-bold tracking-tight font-display uppercase text-agora-pink-light"
+    >
+      Agora Space Dao
+    </h2>
+    <p class="font-semibold 2xl:text-2xl text-agora-white">
+      Our tools are collectively governed by a group of the most experienced Web3 builders, token
+      community managers and DAOs.
+    </p>
+  </div>
 </section>
