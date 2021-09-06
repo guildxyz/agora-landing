d47c2a0e15eac368d3da661916e9b403ece5f0f8
KovJonas <kovacsjonas99@gmail.com>
2021-09-06 11:18:22 +0200

    Revert "Switched to Vercel adapter"
    This reverts commit 79ef503c599a78fb355b05beb224e381b46c90f2.

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import preprocess from 'svelte-preprocess';
-// import adapter from '@sveltejs/adapter-static';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +10,7 @@ const config = {
   ],
 
   kit: {
-    adapter: vercel(),
+    adapter: adapter(),
     target: '#svelte',
     /* hydrate: false, */
     router: false
