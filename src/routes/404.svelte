a3f181046e5e520af66b1709a685ed441267c7f9
KovJonas <kovacsjonas99@gmail.com>
2021-09-06 12:33:46 +0200

    Added contact us link, fixed some UI issues

--- src/routes/404.svelte
+++ src/routes/404.svelte
@@ -7,9 +7,9 @@
   class="min-h-screen flex flex-col items-center xl:items-start justify-center bg-agora-gray text-agora-white"
 >
   <div
-    class="container h-[80vh] text-center xl:text-left xl:bg-notfound bg-no-repeat bg-top bg-contain"
+    class="container h-[80vh] text-center xl:text-left xl:bg-notfound bg-no-repeat bg-top bg-cover"
   >
-    <div class="mx-auto xl:mx-0 max-w-sm">
+    <div class="flex flex-col mx-auto xl:mx-0 max-w-sm h-full">
       <h1
         class="mb-8 text-3xl md:text-4xl xl:text-5xl font-bold tracking-tight font-display uppercase text-agora-pink-medium"
       >
@@ -22,6 +22,11 @@
         <span>Go Back</span>
         <ArrowRight slot="icon" weight="bold" size="1.2em" />
       </Button>
+      <a
+        href="https://discord.gg/T3Pb7VkUBz"
+        target="_blank"
+        class="mt-auto mb-4 font-semibold 2xl:text-2xl">Contact us</a
+      >
     </div>
   </div>
 </div>
