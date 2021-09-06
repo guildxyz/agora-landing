60651c95287e5fa2ff718eda63a6cf5553546e93
KovJonas <kovacsjonas99@gmail.com>
2021-09-06 08:26:11 +0200

    Error page - load function

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,1 +1,16 @@
-<h1 class="pt-20 font-display font-bold tracking-tight text-2xl text-center">An error occurred</h1>
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: {
+        title: `${status}: ${error.message}`
+      }
+    };
+  }
+</script>
+
+<script>
+  export let title;
+</script>
+
+<h1 class="pt-20 font-display font-bold tracking-tight text-2xl text-center">An error occurred!</h1>
+<p class="text-center">{title}</p>
