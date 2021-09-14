c7355be51e26485c1babb56f5d1ba4f0d1f4a4d1
KovJonas <kovacsjonas99@gmail.com>
2021-09-14 08:20:10 +0200

    Key technologies section update

--- src/lib/sections/KeyTechnologies.svelte
+++ src/lib/sections/KeyTechnologies.svelte
@@ -1,51 +1,66 @@
 <section id="key-technologies" class="relative bg-agora-blue-medium text-agora-white">
-  <div class="container relative px-8 py-24 text-center lg:text-left">
-    <div class="mx-auto lg:mx-0 max-w-lg xl:max-w-screen-sm">
+  <div class="container relative px-8 pt-24 lg:pb-24 text-center">
+    <div
+      class="mx-auto lg:mx-0 max-w-lg lg:max-w-screen-md lg:text-left lg:grid lg:grid-cols-2 lg:gap-16"
+    >
       <h2
-        class="mb-16 text-3xl lg:text-4xl 2xl:text-5xl font-bold tracking-tight font-display uppercase text-agora-pink-light"
+        class="mb-16 text-3xl lg:text-4xl 2xl:text-5xl font-bold tracking-tight font-display uppercase text-agora-pink-light lg:col-span-2"
       >
         Key technologies
       </h2>
+      <div>
+        <h3
+          class="mb-2 lg:mb-3 text-xl lg:text-2xl font-bold tracking-tight font-display uppercase"
+        >
+          Social Oracle Layer
+        </h3>
+        <p class="mb-8 lg:font-semibold 2xl:text-lg">
+          Two way social media integration Powered by Witnet to the blockchain creating a circular
+          information flow.
+        </p>
 
-      <h3 class="mb-2 lg:mb-3 text-xl lg:text-2xl font-bold tracking-tight font-display uppercase">
-        Social Oracle Layer
-      </h3>
-      <p class="mb-8 lg:font-semibold 2xl:text-lg">
-        Two way social media integration Powered by Witnet to the blockchain creating a circular
-        information flow.
-      </p>
+        <h3
+          class="mb-2 lg:mb-3 text-xl lg:text-2xl font-bold tracking-tight font-display uppercase"
+        >
+          On-chain Privacy
+        </h3>
+        <p class="mb-8 lg:font-semibold 2xl:text-lg">
+          Zero knowledge based self-sovereign identity management system for keeping the link
+          between your social media accounts and wallets private. This allows a private decision
+          making and sybil attack resistency for many cases.
+        </p>
+      </div>
 
-      <h3 class="mb-2 lg:mb-3 text-xl lg:text-2xl font-bold tracking-tight font-display uppercase">
-        On-chain Privacy
-      </h3>
-      <p class="mb-8 lg:font-semibold 2xl:text-lg">
-        Zero knowledge based self-sovereign identity management system for keeping the link between
-        your social media accounts and wallets private. This allows a private decision making and
-        sybil attack resistency for many cases.
-      </p>
+      <div>
+        <h3
+          class="mb-2 lg:mb-3 text-xl lg:text-2xl font-bold tracking-tight font-display uppercase"
+        >
+          Permissionless Tools
+        </h3>
+        <p class="mb-8 lg:font-semibold 2xl:text-lg">Fully open-source, hosted on IPFS.</p>
 
-      <h3 class="mb-2 lg:mb-3 text-xl lg:text-2xl font-bold tracking-tight font-display uppercase">
-        Permissionless Tools
-      </h3>
-      <p class="mb-8 lg:font-semibold 2xl:text-lg">Fully open-source, hosted on IPFS.</p>
+        <h3
+          class="mb-2 lg:mb-3 text-xl lg:text-2xl font-bold tracking-tight font-display uppercase"
+        >
+          Customizable
+        </h3>
+        <p class="mb-8 lg:font-semibold 2xl:text-lg">
+          Spaces can have their own custom domain name and unique appearance.
+        </p>
 
-      <h3 class="mb-2 lg:mb-3 text-xl lg:text-2xl font-bold tracking-tight font-display uppercase">
-        Customizable
-      </h3>
-      <p class="mb-8 lg:font-semibold 2xl:text-lg">
-        Spaces can have their own custom domain name and unique appearance.
-      </p>
-
-      <h3 class="mb-2 lg:mb-3 text-xl lg:text-2xl font-bold tracking-tight font-display uppercase">
-        Multi-chain
-      </h3>
-      <p class="lg:font-semibold 2xl:text-lg">
-        We integrate every major EVM-compatible blockchains and L2s besides Solana and Near.
-      </p>
+        <h3
+          class="mb-2 lg:mb-3 text-xl lg:text-2xl font-bold tracking-tight font-display uppercase"
+        >
+          Multi-chain
+        </h3>
+        <p class="lg:font-semibold 2xl:text-lg">
+          We integrate every major EVM-compatible blockchains and L2s besides Solana and Near.
+        </p>
+      </div>
     </div>
 
     <img
-      class="hidden lg:block absolute bottom-0 right-0 w-3/5"
+      class="lg:absolute lg:bottom-0 lg:-right-20 xl:right-0 w-full lg:w-2/5"
       src="/images/pegasus.png"
       alt="Pegasus"
     />
