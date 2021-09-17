76a0b3d978daddc3cdfc990c7d1854c22584eecd
valid <vali.domokos@gmail.com>
2021-09-17 20:20:03 +0200

    UI improvements

--- src/routes/404.svelte
+++ src/routes/404.svelte
@@ -4,19 +4,19 @@
 </script>
 
 <div
-  class="min-h-screen flex flex-col items-center justify-center px-8 lg_px-0 bg-agora-gray text-agora-white"
+  class="min-h-screen flex flex-col items-center justify-center px-20 lg:px-36 xl:px-44 bg-agora-gray text-agora-white"
 >
-  <div class="relative w-full md:w-auto">
+  <div class="relative w-full md:w-auto max-w-5xl">
     <img class="hidden md:block" src="/images/404.png" alt="404 - Page not found!" />
     <div
       class="md:absolute top-0 left-0 flex flex-col mx-auto xl:mx-0 max-w-sm h-full text-center md:text-left"
     >
       <h1
-        class="mb-8 text-3xl md:text-4xl xl:text-5xl font-bold tracking-tight font-display uppercase text-agora-pink-medium"
+        class="mb-8 text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold tracking-tight font-display uppercase text-agora-pink-medium"
       >
         404<br />Page not found
       </h1>
-      <p class="mb-16 font-semibold 2xl:text-2xl">
+      <p class="mb-8 font-semibold xl:text-xl">
         Sorry, something went wrong and the page you’re looking for cannot be accessed.
       </p>
       <Button href="/" class="mx-auto md:mx-0 max-w-max bg-agora-pink-medium">
@@ -26,7 +26,7 @@
       <a
         href="https://discord.gg/T3Pb7VkUBz"
         target="_blank"
-        class="mt-8 md:mt-auto mb-4 font-semibold 2xl:text-2xl">Contact us</a
+        class="mt-8 md:mt-auto mb-4 font-semibold xl:text-axl">Contact us</a
       >
     </div>
   </div>
